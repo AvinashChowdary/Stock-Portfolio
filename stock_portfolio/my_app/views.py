1169d9650dfc99913118eb0252095a9e30d2a26e
Avinash Ravilla <avinashchowdary0801@gmail.com>
2018-05-16 14:23:08 -0700

    getting details from input to views and redirecting to details page

--- stock_portfolio/my_app/views.py
+++ stock_portfolio/my_app/views.py
@@ -12,11 +12,30 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-
+@csrf_exempt
 def index_view(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
-
+@csrf_exempt
 def details_view(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    if request.method == 'POST':
+        
+        amount = request.POST.get('amount')
+        
+        is_ethical_enabled = request.POST.get('ethical_investing')
+        is_growth_enabled = request.POST.get('griowth_investing')
+        is_index_enabled = request.POST.get('index_investing')
+        is_quality_enabled = request.POST.get('quality_investing')
+        is_value_enabled = request.POST.get('value_investing')
+        
+        context = {
+            "amount" : amount,
+            "is_ethical_enabled" : is_ethical_enabled,
+            "is_growth_enabled" : is_growth_enabled,
+            "is_index_enabled" : is_index_enabled,
+            "is_quality_enabled" : is_quality_enabled,
+            "is_value_enabled" : is_value_enabled
+        }
+        
+        
+        return render(request, "details.html", context)
